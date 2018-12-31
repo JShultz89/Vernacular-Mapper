2ede1af59754b3bf9a09c9ad584955533e1a948d
Justin Shultz <jss5237@gmail.com>
2018-12-31 10:53:59 -0500

    Started adding HTML to Popup
    Changes:
    * Added very simple HTML to the popup.
    
    Future Work:
    * Would like to have the HTML link to a new page with climate specific recommendations.
    * Group weather files by climate zone.

--- VernacularMapperGenerator.py
+++ VernacularMapperGenerator.py
@@ -4,7 +4,7 @@
 import folium
 import pandas
 
-data = pandas.read_csv("data/epw_weather_data.csv")
+data = pandas.read_csv("data/usa.csv")
 lat = list(data["lat"])
 lon = list(data["lon"])
 station = list(data["station_name"])
@@ -17,13 +17,60 @@ station = list(data["station_name"])
 #    else:
 #        return 'red'
 
+def get_frame(url,width,height):
+    html = """ 
+            <!doctype html>
+        <html>
+        <iframe id="myIFrame" width="{}" height="{}" src={}""".format(width,height,url) + """ frameborder="0"></iframe>
+        <script type="text/javascript">
+        var resizeIFrame = function(event) {
+            var loc = document.location;
+            if (event.origin != loc.protocol + '//' + loc.host) return;
+
+            var myIFrame = document.getElementById('myIFrame');
+            if (myIFrame) {
+                myIFrame.style.height = event.data.h + "px";
+                myIFrame.style.width  = event.data.w + "px";
+            }
+        };
+        if (window.addEventListener) {
+            window.addEventListener("message", resizeIFrame, false);
+        } else if (window.attachEvent) {
+            window.attachEvent("onmessage", resizeIFrame);
+        }
+        </script>
+        </html>"""
+
+    popup = get_frame(url,
+                      width,
+                      height)
+
+    marker = folium.CircleMarker([lat,lon],
+                                 radius=radius,
+                                 color='#3186cc',
+                                 fill_color = '#3186cc',
+                                 popup=popup)
+
+    marker.add_to(map)
+
+
+
+    iframe = folium.element.IFrame(html=html,width=width,height=height)
+    popup = folium.Popup(iframe,max_width=width)
+    return popup
+
 map = folium.Map(location=[38.903477, -77.065231], zoom_start=6, tiles="Mapbox Bright")
 
 fgv = folium.FeatureGroup(name="Weather Stations")
 
 for lt, ln, stat in zip(lat, lon, station):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, tooltip=str(stat),
-                                      popup="Cliamte Zone = ", fill_color='grey',
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],
+                                      radius = 6, tooltip=str(stat),
+                                      popup=folium.Popup(html="""
+                                                            <h2>Weather Averages</h2>
+                                                            <a href='https://www.google.com/'>Google Link</a>
+                                                            """),  # get_frame('https://www.google.com/', 150, 150),
+                                      fill_color='grey',
                                       fill=True, fill_opacity=0.7))
 
 #fgp = folium.FeatureGroup(name="Population")
